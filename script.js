3f44a28edb09fbdc0d6f80e92b3fc7227ae1fcb2
Eli Metzler-Prieb <elimetzlerprieb@gmail.com>
2017-03-17 23:23:09 -0400

    Add buildPuzzle and showPuzzle to generate puzzle array and build HTML representation

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
+var puzzleTemplate = buildPuzzle();
+
+// From dailysudoku.com/sudoku/play.shtml on March 17, 2017
+var testPuzzle = [[4,0,8,0,0,0,0,6,7],
+				  [0,2,0,0,0,8,0,0,0],
+				  [0,0,0,7,5,0,0,0,0],
+				  [8,0,5,0,7,4,0,2,0],
+				  [2,0,0,0,0,0,0,0,5],
+				  [0,4,0,5,6,0,3,0,8],
+				  [0,0,0,0,1,9,0,0,0],
+				  [0,0,0,8,0,0,0,9,0],
+				  [5,6,0,0,0,0,1,0,3]];
+
+
 $(document).ready(function(){
 
-	var masterPuzzle = buildPuzzle();
-	showPuzzle();
-
+	showPuzzle(puzzleTemplate);
+	console.log(testPuzzle);
 
 })
 
 // Creates an array(rows) of arrays (column values) representing a sudoku puzzle
 
 function buildPuzzle() {
-	var arr = [];
+	var puzzle = [];
 	var i = 1;
 	for(var y=0; y<9; y++) {
-		arr[y] = [];
+		puzzle[y] = [];
 		for(var x=0; x<9; x++) {
-			arr[y][x] = i;
-			i++;
+			puzzle[y][x] = 0;
 		}
 	}
-	
-	console.log(arr);
+	return puzzle;
 };
 
-// Uses the above array to construct html elements representing the puzzle
+// Creates HTML array with row id mapped to row and td id mapped to column
 
-function showPuzzle() {
+function showPuzzle(puzzleTemplate) {
+	// Append rows to table(id=puzzle)
+	for(var x = 0; x < (puzzleTemplate.length); x++) {
+		jQuery("<tr/>", {
+			id: x,
+		}).appendTo("#puzzle");
+	}
+
+	// Append td to each row
+	for(var y = 0; y < (puzzleTemplate.length); y++) {
+		jQuery("<td/>", {
+		class: "box",
+		id: y,	
+		}).appendTo("tr");
+	}
+};
+
+function solvePuzzle(puzzle) {
 
 };
