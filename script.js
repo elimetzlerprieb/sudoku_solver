ebabd5b663893742ddec4a416c0320c6838f0d77
Eli Metzler-Prieb <elimetzlerprieb@gmail.com>
2017-03-18 17:57:29 -0400

    Add validity check function for column, row, 3x3 grid. Add checkEmpty to find empty cells.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var puzzleTemplate = buildPuzzle();
+var templatePuzzle = buildPuzzle();
 
 // From dailysudoku.com/sudoku/play.shtml on March 17, 2017
 var testPuzzle = [[4,0,8,0,0,0,0,6,7],
@@ -11,16 +11,15 @@ var testPuzzle = [[4,0,8,0,0,0,0,6,7],
 				  [0,0,0,8,0,0,0,9,0],
 				  [5,6,0,0,0,0,1,0,3]];
 
+console.log(testPuzzle);
 
 $(document).ready(function(){
 
-	showPuzzle(puzzleTemplate);
-	console.log(testPuzzle);
-
+	showPuzzle(templatePuzzle);
+	clickToSolve(testPuzzle);
 })
 
 // Creates an array(rows) of arrays (column values) representing a sudoku puzzle
-
 function buildPuzzle() {
 	var puzzle = [];
 	var i = 1;
@@ -34,17 +33,16 @@ function buildPuzzle() {
 };
 
 // Creates HTML array with row id mapped to row and td id mapped to column
-
-function showPuzzle(puzzleTemplate) {
+function showPuzzle(puzzle) {
 	// Append rows to table(id=puzzle)
-	for(var x = 0; x < (puzzleTemplate.length); x++) {
+	for(var x = 0; x < (puzzle.length); x++) {
 		jQuery("<tr/>", {
 			id: x,
 		}).appendTo("#puzzle");
 	}
 
 	// Append td to each row
-	for(var y = 0; y < (puzzleTemplate.length); y++) {
+	for(var y = 0; y < (puzzle.length); y++) {
 		jQuery("<td/>", {
 		class: "box",
 		id: y,	
@@ -52,6 +50,135 @@ function showPuzzle(puzzleTemplate) {
 	}
 };
 
+function clickToSolve(puzzle) {
+	$("#solve").click(function() {
+		var solvedPuzzle = solvePuzzle(puzzle);
+		console.log(solvedPuzzle);
+	})
+};
+
+// Recursively solve the sudoku puzzle using a backtracking method
 function solvePuzzle(puzzle) {
+	var coord = findEmpty(puzzle);
+
+	if(coord == false){
+		console.log("Sudoku Solved!")
+		console.log(puzzle)
+		return
+	}
+	
+	x = coord[0];
+	y = coord[1];
+
+	console.log(x)
+	console.log(y)
+
+	copyPuzzle = puzzle;
+
+	for(var i = 1; i < 10; i++){
+		if(i==10){return false}
+
+		copyPuzzle[x][y] = i;
+		if((checkRow(copyPuzzle,x,y,i)==false) || (checkColumn(copyPuzzle,x,y,i)==false) || (check3x3(copyPuzzle,x,y,i)==false)) {
+			continue
+		} else if((checkRow(copyPuzzle,x,y,i)==true) && (checkColumn(copyPuzzle,x,y,i)==true) && (check3x3(copyPuzzle,x,y,i)==true)) {
+			solvePuzzle(copyPuzzle);
+			continue
+		} 
+	}
+
+
+	console.log(puzzle)
+};
+ 
+function findEmpty(puzzle){
+	for(var x = 0; x < (puzzle.length); x++) {
+		for(var y = 0; y < (puzzle.length); y++) {
+			if (puzzle[x][y] == 0) {
+				return[x, y]
+			}
+		}
+	}
+	return false
+};
+
+/*
+function assignValue(puzzle,x,y){
+	for(var i = 1; i < 10; i++){
+		puzzle[x][y] = i;
+		if((checkRow(puzzle,x,y,i)==false) || (checkColumn(puzzle,x,y,i)==false) || (check3x3(puzzle,x,y,i)==false)) {
+			continue;
+		} else if((checkRow(puzzle,x,y,i)==true) && (checkColumn(puzzle,x,y,i)==true) && (check3x3(puzzle,x,y,i)==true)) {
+			solvePuzzle(puzzle);
+			continue;
+		} else {return false}
+	}
+};
+*/
+
+function checkRow(puzzle,x,y,i){
+	for(var j = 0; j < 9; j++){
+		if(j==y){
+			continue
+		} else if(puzzle[x][j] == i){
+			return false;
+		}
+	}
+	return true;
 
 };
+
+function checkColumn(puzzle,x,y,i){
+	for(var k = 0; k < 9; k++){
+		if(k==x){
+			continue
+		} else if(puzzle[k][y] == i){
+			return false
+		}
+	}
+	return true
+};
+
+function check3x3(puzzle,x,y,i){
+	if ((x<3) && (y<3)){
+		xx = 3;
+		yy = 3;
+	} else if ((x<3) && (y<6)){ //Grid2
+		xx = 3;
+		yy = 6;
+	} else if ((x<3) && (y<9)){ //Grid3
+		xx = 3;
+		yy = 9;
+	} else if ((x<6) && (y<3)){ //Grid4
+		xx = 6;
+		yy = 3;
+	} else if ((x<6) && (y<6)){ //Grid5
+		xx = 6;
+		yy = 6;
+	} else if ((x<6) && (y<9)){ //Grid6
+		xx = 6;
+		yy = 9;
+	} else if ((x<9) && (y<3)){ //Grid7
+		xx = 9;
+		yy = 3;
+	} else if ((x<9) && (y<6)){ //Grid8
+		xx = 9;
+		yy = 6;
+	} else if ((x<9) && (y<9)){ //Grid9
+		xx = 9;
+		yy = 9;
+	}	
+
+	for (var j = (xx-3); j < xx; j++) {
+		for (var k = (yy-3); k < yy; k++) {
+			if((x==j)&&(y==k)) {
+				continue
+			} else if(puzzle[j][k] == i) {
+				return false
+			}
+		}
+	}
+	return true 
+};
+
+
